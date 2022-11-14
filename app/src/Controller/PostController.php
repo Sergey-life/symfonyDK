c36b3f06c7ff6d1045a36de2dec4061b05313be4
Sergey-life <sergei.kharchenko888@gmail.com>
2022-11-14 22:41:21 +0200

    Connect to DB and create Entities

--- app/src/Controller/PostController.php
+++ app/src/Controller/PostController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
 
 class PostController extends AbstractController
 {
     #[Route('/post', name: 'app_post')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        $test = 'test';
-        return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
-        ]);
+        $entityManager = $doctrine->getManager();
+
+        $post = new Post();
+        $post->setTitle('Топ 10 красивих акордів на гітарі');
+        $post->setBody('Am7, Em7, G, H7, ,Dm7 Fsus, F#m, G#7, C, Fmaj');
+        $post->setImage('test.img');
+        $post->setSlug('top-10-beautiful-guitar-chords');
+        $post->setTopicId(1);
+
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return new Response('Пост з id: '.$post->getId().' успішно збережений!');
+
+//        return $this->render('post/index.html.twig', [
+//            'postId' => $post->getId(),
+//        ]);
     }
 }
---
+++ app/src/Entity/Post.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $body = null;
+
+    #[ORM\Column(length: 120)]
+    private ?string $slug = null;
+
+    #[ORM\Column]
+    private ?int $topic_id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getTopicId(): ?int
+    {
+        return $this->topic_id;
+    }
+
+    public function setTopicId(int $topic_id): self
+    {
+        $this->topic_id = $topic_id;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
